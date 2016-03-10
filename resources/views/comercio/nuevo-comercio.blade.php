473fd0b53be2426f1af998a11a777b1a3bbe3233
Juan Subiros <juanpablosubiros@avalith.net>
2016-03-10 13:02:58 -0300

    commit1

--- resources/views/comercio/nuevo-comercio.blade.php
+++ resources/views/comercio/nuevo-comercio.blade.php
@@ -10,10 +10,11 @@
         <h1>Nuevo Comercio</h1>
         <div class="wrap comercio">
             <form action="/alta-comercio" method="POST" class="form-horizontal">
-                <div class="input-group"><label for="nombre">Nombre</label><input type="text" id="nombre" name="nombre"></div>
-                <div class="input-group"><label for="email">Email</label><input type="text" id="email" name="email"></div>
-                <div class="input-group"><label for="pass">Contraseña</label><input type="password" id="pass" name="pass"></div>
-                <div class="input-group"><label for="pass2">Confirmar</label><input type="password" id="pass2" name="pass2"></div>
+                <div class="form-group"><label for="nombre" class="col-md-3">Nombre</label><input type="text" id="nombre" name="nombre" ></div>
+                <div class="form-group"><label for="email" class="col-md-3">Email</label><input type="text" id="email" name="email" ></div>
+                <div class="form-group"><label for="image" class="col-md-3" style="display: inline-block;">Imagen</label><input type="file" id="image" name="image" style="display: inline-block;" ></div>
+                <div class="form-group"><label class="col-md-3" for="pass">Contraseña</label><input type="password" id="pass" name="pass" ></div>
+                <div class="form-group"><label class="col-md-3" for="pass2">Confirmar</label><input type="password" id="pass2" name="pass2" ></div>
                 <div class="text-center"><input type="checkbox"><label for="">Acepto Terminos</label></div>
                 <div class="text-center"><button type="submit" class="btn btn-default">Suscribite</button></div>
             </form>
