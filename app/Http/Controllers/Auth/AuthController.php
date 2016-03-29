52a2d59b219fdf098e2581d62c75cf022fc51c6c
Juan Subiros <juanpablosubiros@avalith.net>
2016-03-29 16:42:36 -0300

    problemas login resueltos

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -90,5 +90,17 @@ class AuthController extends Controller
         ]);
     }
 
+    public function postRegister(Request $request)
+    {
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+        
+        return redirect($this->redirectPath());
+    }
+
     
 }
